6f504cc65e8cd02c7b581ef32cdfbb191bb8e4f8
Amobi-blaise <amobiblaise88@gmail.comgit config --global user.email amobiblaise88@gmail.com>
2022-04-08 14:51:37 -0700

    C Commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int n
+
+	if( n > 0){
+		printf("n is positive\n");
+	}
+	else if( n == 0){
+		printf("n is zero\n");
+	}
+	else {
+		printf("n is negative\n");
+	return 0
+
+}
