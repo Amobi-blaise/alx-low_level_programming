fcdc8ad387f2b37eb83c2614bf033bc4eea26e8a
Amobi-blaise <amobiblaise88@gmail.comgit config --global user.email amobiblaise88@gmail.com>
2022-04-10 04:06:50 -0700

    C Commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - The containing function of this program
+ * Return: this gives 0 successful
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0)
 }
