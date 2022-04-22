78b04a090d23687c04b14b72ef086e982e2bf048
Amobi-blaise <amobiblaise88@gmail.comgit config --global user.email amobiblaise88@gmail.com>
2022-04-22 13:00:22 -0700

    Nested loop

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ * Return: 1 and print + if greater than zero, 0 and print 0 if equal to zero, -1 and print - if less than zero
+ */
+int print_sign(int n)
+{
+
