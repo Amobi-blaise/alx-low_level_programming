3cb918f1397cc5bd24ab5ead6c879df86d015340
Amobi-blaise <amobiblaise88@gmail.comgit config --global user.email amobiblaise88@gmail.com>
2022-04-24 04:38:11 -0700

    More nested loop

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _isupper - checks uppercase
- * @c: print int
- * Return: give 1 or 0
+ * _isupper - function that checks for uppercase character
+ * @c: takes in a character
+ * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
