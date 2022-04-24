5347ad2a265e1277f74980aa92863a873b5513a9
Amobi-blaise <amobiblaise88@gmail.comgit config --global user.email amobiblaise88@gmail.com>
2022-04-24 08:27:30 -0700

    More nested loop

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,7 @@
 /**
  * _isupper - function that checks for uppercase character
  * @c: takes in a character
- * Return: 1 if c is uppercase, 0 otherwise
+ * Return: if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
