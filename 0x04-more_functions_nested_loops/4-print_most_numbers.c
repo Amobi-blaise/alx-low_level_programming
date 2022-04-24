fa26cf522e6b20380aca69ad331e82ff2746441f
Amobi-blaise <amobiblaise88@gmail.comgit config --global user.email amobiblaise88@gmail.com>
2022-04-24 11:34:24 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,25 +2,19 @@
 /**
  * print_most_numbers - prints except 2 and 4
  *
- * Retuen: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 void print_most_numbers(void)
 {
-int i;
+	int i;
+	int = 0;
 
-i = 0;
-
-for (i = 0; i < 10; i++)
-{
-
-if (i == 2 || i == 4);
-
-i++;
-
-_putchar(i + '0');
-
-}
-_putchar ('\n');
-
+	for (i = o; i < 10; i++)
+	{
+		if (i == 2 || i == 4)
+			i++;
+		_putchar(i + '0');
+	}
+	_putchar('\n');
 }
