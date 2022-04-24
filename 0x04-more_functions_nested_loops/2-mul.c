a393b183645984b5a3a7fa6fb2d74b16a805ff57
Amobi-blaise <amobiblaise88@gmail.comgit config --global user.email amobiblaise88@gmail.com>
2022-04-24 08:51:01 -0700

    More nested loop

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,9 +3,9 @@
  * _mul - a function that multiplies two integers
  * @a: first input
  * @b: second input
- * Returns: multiplication result of the 2 inputs
+ * Return: multiplication result of the 2 inputs
  */
 int mul(int a, int b)
 {
-  return (a * b);
+	return (a * b);
 }
