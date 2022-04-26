78a040c3e9565cc4ab0abc61c2a392c5f5f5269e
Amobi-blaise <amobiblaise88@gmail.comgit config --global user.email amobiblaise88@gmail.com>
2022-04-26 14:53:15 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _memset - a function that fills memory with a constant type
+ * _memset - a function that fills memory with a constant byte
  *
  * @s: input pointer to char type
  *    represents the pointer to the block of memory to fill
