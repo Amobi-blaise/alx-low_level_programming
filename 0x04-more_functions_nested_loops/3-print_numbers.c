42d6b1081d7e7479e707c751e08673e642326768
Amobi-blaise <amobiblaise88@gmail.comgit config --global user.email amobiblaise88@gmail.com>
2022-04-26 17:57:57 -0700

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,18 @@
 #include "main.h"
+
 /**
- * print_numbers - a function that prints numbers, from 0 to 9
- * Return: 0-9 followed by newline
+ * print_number - print the numbers since 0 up to 9
+ *
+ * Return: The numbers since 0 up to 9
  */
 void print_numbers(void)
 {
-	int i;
-	i = 0;
+	int x;
 
-	for (i = 0; i < 10; i++)
-		_putchar(i + '0');
+	for (x = 0; x <= 9; x++)
+	{
+		_putchar(x + '0');
+	}
+
 	_putchar('\n');
 }
